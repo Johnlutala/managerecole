341cfae011c5895c8c9d4deefd5aaa30821cb276
LeonardoRubuz <rubuzleonardo@gmail.com>
2026-06-23 12:58:55 +0100

    supprimer la contrainte de validation Regex pour le shortcode

--- src/Dto/Api/MerchantConfigPayload.php
+++ src/Dto/Api/MerchantConfigPayload.php
@@ -7,10 +7,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 class MerchantConfigPayload
 {
     #[Assert\NotBlank(message: "Code marchand obligatoire")]
-    #[Assert\Regex(
-        pattern: "/^[a-zA-Z0-9]+$/",
-        message: "Le code marchand doit être alphanumérique"
-    )]
     private string $shortcode;
 
 
